17593e7c0c89b76427cb08e2b8cc4d46c2a2d76c
Boaz Rosenan <brosenan@gmail.com>
2017-02-16 21:50:47 +0200

    1. travis fix; 2. Rearranging documentation

--- test/pureclj/core_spec.clj
+++ test/pureclj/core_spec.clj
@@ -2,86 +2,99 @@
   (:require [pureclj.core :refer :all]
             [midje.sweet :refer :all]))
 
-[[:chapter "symbols: Extract symbols from expressions"]]
-"should return an empty set for a constant"
+[[:chapter {:title "Introduction"}]]
+"Like other LISPs, Clojure is an imperative functional programming language.
+In many ways, Clojure has made strides towards 'purity', such as intensive use
+of purely-functional data structures, but in other areas, such as Java interop,
+Clojure is as imperative as Java.
+While imperative programming is commonplace, with only few esoteric programming languages
+taking the purely-declarative path, it has some disadvantages."
+
+"pureclj is an attempt to create a purely-functional dialect of Clojure.
+This is a stripped-down Clojure, with only the purely-functional parts, including:
+- Functions (well dah...)
+- `let`, `loop`, `if` etc.
+- maps, sets, vectors and lists
+- A subset of the `clojure.core` library functions and macros, as well as
+- `clojure.string`, `clojure.set` and potentially other pure libraries.
+
+It does not include:
+- Variables
+- Atoms, refs, agents, etc.
+- Java interop
+- Imperative functions from `clojure.core`"
+
+[[:chapter {:title "symbols: Extract symbols from expressions"}]]
+"The `symbols` function is the key in our static analysis.  It takes a s-expression,
+and returns a set of symbols defined by this s-expression."
+
+"For Constants, it returns an empty set."
 (fact 
- (symbols 2) => #{})
-"should return an empty set for a boolean"
-(fact 
- (symbols true) => #{})
-"should return an empty set for a keyword"
-(fact
- (symbols :foo) => #{})
-"should return en empty set for nil"
-(fact
+ (symbols 2) => #{}
+ (symbols true) => #{}
+ (symbols "foo") => #{}
+ (symbols :foo) => #{}
  (symbols nil) => #{})
-"should return an empty set for a string literal"
-(fact 
- (symbols "foo") => #{})
-"should return a singleton set for a symbol"
+
+"For a symbol, `symbols` returns a set with that symbol."
 (fact
  (symbols 'x) => #{'x})
-"should reconstruct lists"
+"`symbols` goes into Clojure structures and aggergates symbols from there."
 (fact
- (symbols '(+ a b)) => #{'+ 'a 'b})
-"should reconstruct a vector"
+ (symbols '(+ a b)) => #{'+ 'a 'b}
+ (symbols '[1 a 2 b]) => #{'a 'b}
+ (symbols {'x 'y}) => #{'x 'y}
+ (symbols #{'a 'b 3 4 5}) => #{'a 'b})
+
+"In a `let*` special form, the bindings are removed from the returned set."
 (fact
- (symbols '[1 a 2 b]) => #{'a 'b} )
-"should reconstruct a map"
-(fact
- (symbols {'x 'y}) => #{'x 'y} )
-"should reconstruct a set"
-(fact
- (symbols #{'a 'b 3 4 5}) => #{'a 'b} )
-"should remove bound variables in a let* form"
-(fact
- (symbols '(let* [x 1 y 2] (+ x y))) => #{'+} )
-"should add symbols in bindings expressions"
-(fact
- (symbols '(let* [x a] x)) => #{'a} )
-"should apply macros"
+ (symbols '(let* [x 1 y 2] (+ x y))) => #{'+}
+ (symbols '(let* [x a] x)) => #{'a})
+
+"`symbols` expands macros, so it also handles the more familiar `let` form"
 (fact
  (symbols '(let [x a] x a)) => #{'a} )
-"should remove symbols from an unnamed fn* arg list"
+
+"`loop` is supported similarly."
 (fact
- (symbols '(fn [x y] (+ x y a))) => #{'a '+} )
-"should remove symbols from a named fn*"
+ (symbols '(loop [x a y b] a x b y c)) => #{'a 'b 'c} )
+
+"In the `fn*` special form (used by the `fn` macro) `symbols` removes the argument names from the set."
 (fact
- (symbols '(fn foo [x y] (+ x y a))) => #{'a '+} )
-"should remove the function name in case of a recursive call"
+ (symbols '(fn [x y] (+ x y a))) => #{'a '+}
+ (symbols '(fn foo [x y] (+ x y a))) => #{'a '+})
+
+"The function name (in named functions) is also removed."
 (fact
  (symbols '(fn foo [x] (foo x))) => #{} )
-"should support multi-clause functions"
+
+"Multi-arity functions are supported as well."
 (fact
  (symbols '(fn ([x] (+ a x))
              ([x y] (+ b x y)))) => #{'a 'b '+} )
-"should throw an exception on a def form"
+"`def` is not allowed inside an expression (it is allowed on the top-level, as described below)."
 (fact
  (symbols '(def x 2)) => (throws "def is not allowed") )
-"should ignore the contents of a quote"
-(fact
- (symbols ''(a b c)) => #{} )
-"should ignore the 'if' of an if form"
-(fact
- (symbols '(if a b c)) => #{'a 'b 'c} )
-"should ignore the 'do' of a do form"
-(fact
- (symbols '(do a b c)) => #{'a 'b 'c} )
-"should throw an exception on a var form"
+
+"Similarly, reference to variables is not allowed."
 (fact
  (symbols '#'var) => (throws "vars are not allowed") )
-"should handle the bindings of a loop"
+
+"Quoted symbols are ignored."
 (fact
- (symbols '(loop [x a y b] a x b y c)) => #{'a 'b 'c} )
-"should ignore the 'recur' in a recur form"
+ (symbols ''(a b c)) => #{} )
+
+"In special forms such as `if` and `do`, the form's name is ignored."
 (fact
- (symbols '(recur a b)) => #{'a 'b} )
-"should throw an exception on a throw form"
+ (symbols '(if a b c)) => #{'a 'b 'c}
+ (symbols '(do a b c)) => #{'a 'b 'c}
+ (symbols '(recur a b)) => #{'a 'b})
+
+"Exceptions are not supported because they use Java interop."
 (fact
- (symbols '(throw foo)) => (throws Exception "throw is not allowed. Use error instead") )
-"should throw an exception on a try form"
-(fact
- (symbols '(try foo bar baz)) => (throws "try/catch is not allowed") )
+ (symbols '(throw foo)) => (throws Exception "throw is not allowed. Use error instead")
+ (symbols '(try foo bar baz)) => (throws "try/catch is not allowed"))
+
 
 [[:chapter {:title "box: Evaluate expressions through an environment" :tag "box"}]]
 "should return a constant for a constant"
